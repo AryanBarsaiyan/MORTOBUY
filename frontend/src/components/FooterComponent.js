7c14811629e11443a99096c3227c893a8e7a73aa
Aryan Barsaiyan <82722871+AryanBarsaiyan@users.noreply.github.com>
2023-02-19 17:02:53 +0530

    h

---
+++ frontend/src/components/FooterComponent.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const FooterComponent = () => {
+  return (
+    <footer>
+      <Container fluid>
+        <Row className="mt-5">
+          <Col className="bg-dark text-white text-center py-5">
+            Copyright &copy; AB Creactions
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default FooterComponent;
